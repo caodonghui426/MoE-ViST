be1ea70c379e9fd78581028555887e569c5472dd
caodonghui426 <cao_dong_hui@163.com>
2024-04-15 14:45:53 +0800

    update MoeViST, vision_transformer(moe部分)

--- vilt/modules/vision_transformer.py
+++ vilt/modules/vision_transformer.py
@@ -271,7 +271,7 @@ class MoE(nn.Module):
         out_features=None,
         act_layer=nn.GELU,
         drop=0.0,
-        num_experts=4,
+        num_experts=[4],
         ep_size=2, # Size of expert parallel world (should be less than total world size)
         use_residual=False,
         top_k=1, # default=1, type=int, help="(moe) gating top 1 and 2 supported"
@@ -279,7 +279,12 @@ class MoE(nn.Module):
         noisy_gate_policy=None, # (moe) noisy gating (only supported with top-1). Valid values are None, RSample, and Jitter
     ):
         super().__init__()
-        expert_mlp = Mlp() # TODO
+        self.expert_mlp = Mlp(
+            in_features=in_features,
+            hidden_features=hidden_features,
+            act_layer=act_layer,
+            drop=drop,
+        ) 
         self.moe_layer_list = []
         
         for n_e in num_experts:
@@ -287,7 +292,7 @@ class MoE(nn.Module):
             self.moe_layer_list.append(
                 deepspeed.moe.layer.Moe(
                     hidden_size=in_features,
-                    expert=expert_mlp,
+                    expert=self.expert_mlp,
                     num_experts=n_e,
                     ep_size=ep_size,
                     use_residual=use_residual,
@@ -298,6 +303,12 @@ class MoE(nn.Module):
             )
         self.moe_layer_list = nn.ModuleList(self.moe_layer_list)
         # TODO
+
+    def forward(self, x):
+        for layer in self.moe_layer_list:
+            x, _, _ = layer(x)
+            # TODO (output, loss, gate_values)其中，output是经过所有专家处理后的张量，loss是一个可能用于模型训练的额外损失项（例如，专家的负载均衡损失），gate_values是一个表示输入如何在不同专家间分配的张量。
+        return x
 
         
 
@@ -488,6 +499,69 @@ class Block(nn.Module):
         x = x + self.drop_path(_x)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x, attn
+
+
+class BlockForMoeViST(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_heads,
+        mlp_ratio=4.0,
+        qkv_bias=False,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.norm_image = norm_layer(dim)
+        self.norm_sensor = norm_layer(dim)
+        self.attn = AttentionForViST(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(
+            drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2_image = norm_layer(dim)
+        self.norm2_sensor = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        # TODO 修改MoE参数传输方式（非固定）
+        self.moe_image = MoE(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            act_layer=act_layer,
+            drop=drop,
+        )
+        self.moe_sensor = MoE(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            act_layer=act_layer,
+            drop=drop,
+        )
+
+    def forward(self, x_image, x_sensor):
+        _x_image, _x_sensor, attn_image, attn_sensor = self.attn(
+            self.norm_image(x_image), self.norm_sensor(x_sensor))
+
+        x_image = x_image + self.drop_path(_x_image)
+        x_sensor = x_sensor + self.drop_path(_x_sensor)
+        # x_image = x_image * self.drop_path(_x_image)
+        # x_sensor = x_sensor * self.drop_path(_x_sensor)
+        # x_image = x_image + x_image * self.drop_path(_x_image)
+        # x_sensor = x_sensor + x_sensor * self.drop_path(_x_sensor)
+
+        x_image = x_image + \
+            self.drop_path(self.moe_image(self.norm2_image(x_image)))
+        x_sensor = x_sensor + \
+            self.drop_path(self.moe_sensor(self.norm2_sensor(x_sensor)))
+        return x_image, x_sensor, attn_image, attn_sensor
 
 
 class BlockForViST(nn.Module):
@@ -767,6 +841,316 @@ class VisionTransformer(nn.Module):
         pos_embed = pos_embed.flatten(2).transpose(
             1, 2)  # torch.Size([2, 144, 768])
         x = x.flatten(2).transpose(1, 2)  # torch.Size([32, 144, 768])
+        patch_index = (
+            torch.stack(
+                torch.meshgrid(
+                    torch.arange(x_mask.shape[-2]
+                                 ), torch.arange(x_mask.shape[-1])
+                ),
+                dim=-1,
+            )[None, None, :, :, :]
+            .expand(x_mask.shape[0], x_mask.shape[1], -1, -1, -1)
+            .flatten(1, 3)
+        )
+        x_mask = x_mask.flatten(1)
+
+        if mask_it:
+            x, label = self.mask_tokens(_x, x)
+
+        if (
+            max_image_len < 0
+            or max_image_len is None
+            or not isinstance(max_image_len, int)
+        ):
+            # suppose aug is 800 x 1333, then, maximum effective res is 800 x 1333 (if one side gets bigger, the other will be constrained and be shrinked)
+            # (800 // self.patch_size) * (1333 // self.patch_size) is the maximum number of patches that single image can get.
+            # if self.patch_size = 32, 25 * 41 = 1025
+            # if res is 384 x 640, 12 * 20 = 240
+            eff = x_h * x_w
+            max_image_len = eff.max()
+        else:
+            eff = x_h * x_w
+            max_image_len = min(eff.max(), max_image_len)
+
+        valid_idx = x_mask.nonzero(as_tuple=False)
+        non_valid_idx = (1 - x_mask).nonzero(as_tuple=False)
+        unique_rows = valid_idx[:, 0].unique()
+        valid_row_idx = [valid_idx[valid_idx[:, 0] == u] for u in unique_rows]
+        non_valid_row_idx = [
+            non_valid_idx[non_valid_idx[:, 0] == u] for u in unique_rows
+        ]
+
+        valid_nums = [v.size(0) for v in valid_row_idx]
+        non_valid_nums = [v.size(0) for v in non_valid_row_idx]
+        pad_nums = [max_image_len - v for v in valid_nums]
+
+        select = list()
+        for i, (v, nv, p) in enumerate(zip(valid_nums, non_valid_nums, pad_nums)):
+            if p <= 0:
+                valid_choice = torch.multinomial(
+                    torch.ones(v).float(), max_image_len)
+                select.append(valid_row_idx[i][valid_choice])
+            else:
+                pad_choice = torch.multinomial(
+                    torch.ones(nv).float(), p, replacement=True
+                )
+                select.append(
+                    torch.cat(
+                        [valid_row_idx[i], non_valid_row_idx[i][pad_choice]], dim=0,
+                    )
+                )
+
+        select = torch.cat(select, dim=0)
+        x = x[select[:, 0], select[:, 1]].view(B, -1, C)
+        x_mask = x_mask[select[:, 0], select[:, 1]].view(B, -1)
+        patch_index = patch_index[select[:, 0], select[:, 1]].view(B, -1, 2)
+        pos_embed = pos_embed[select[:, 0], select[:, 1]].view(B, -1, C)
+
+        if mask_it:
+            label = label[select[:, 0], select[:, 1]].view(B, -1, 3)
+
+            label[x_mask == 0] = -100
+            label = torch.cat(
+                [torch.full((label.shape[0], 1, 3), -100).to(label), label, ], dim=1,
+            )
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        pos_embed = torch.cat(
+            (self.pos_embed[:, 0, :][:, None, :].expand(B, -1, -1), pos_embed), dim=1
+        )
+        x = x + pos_embed
+        x = self.pos_drop(x)
+
+        if self.add_norm_before_transformer:
+            x = self.pre_norm(x)
+
+        x_mask = torch.cat(
+            [torch.ones(x_mask.shape[0], 1).to(x_mask), x_mask], dim=1)
+
+        if mask_it:
+            return x, x_mask, (patch_index, (H, W)), label
+        else:
+            return x, x_mask, (patch_index, (H, W)), None
+
+    def forward_features(self, _x, max_image_len=144, mask_it=False):
+        x, x_mask, patch_index, label = self.visual_embed(
+            _x, max_image_len=max_image_len, mask_it=mask_it
+        )
+
+        for blk in self.blocks:
+            x, _ = blk(x, mask=x_mask)
+
+        x = self.norm(x)
+        return x, x_mask, label
+
+    def forward(self, x, max_image_len=-1):
+        x, _, _ = self.forward_features(x, max_image_len=max_image_len)
+        x = x[:, 0]
+        x = self.head(x)
+        return x
+
+
+class VisionTransformerForMoeViST(nn.Module):
+    """ Vision Transformer
+
+    A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`  -
+        https://arxiv.org/abs/2010.11929
+    update for ViST
+    """
+
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        num_classes=1000,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        representation_size=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.0,
+        norm_layer=None,
+        add_norm_before_transformer=False,
+        no_patch_embed_bias=False,
+        config=None,
+    ):
+        """
+        Args:
+            img_size (int, tuple): input image size
+            patch_size (int, tuple): patch size
+            in_chans (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (int): embedding dimension
+            depth (int): depth of transformer
+            num_heads (int): number of attention heads
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            drop_rate (float): dropout rate
+            attn_drop_rate (float): attention dropout rate
+            drop_path_rate (float): stochastic depth rate
+            hybrid_backbone (nn.Module): CNN backbone to use in-place of PatchEmbed module
+            norm_layer: (nn.Module): normalization layer
+        """
+        super().__init__()
+        drop_rate = drop_rate if config is None else config["drop_rate"]
+
+        self.num_classes = num_classes
+        self.num_features = (
+            self.embed_dim
+        ) = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        self.add_norm_before_transformer = add_norm_before_transformer
+
+        self.patch_embed = PatchEmbed(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+        )
+        num_patches = self.patch_embed.num_patches
+
+        self.patch_size = patch_size
+        self.patch_dim = img_size // patch_size
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1, num_patches + 1, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        if add_norm_before_transformer:
+            self.pre_norm = norm_layer(embed_dim)
+
+        dpr = [
+            x.item() for x in torch.linspace(0, drop_path_rate, depth)
+        ]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList(
+            [
+                BlockForMoeViST(
+                    dim=embed_dim,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[i],
+                    norm_layer=norm_layer,
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = norm_layer(embed_dim)
+
+        trunc_normal_(self.pos_embed, std=0.02)
+        trunc_normal_(self.cls_token, std=0.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {"pos_embed", "cls_token"}
+
+    def mask_tokens(self, orig_image, feats):
+        """
+        Prepare masked tokens inputs/labels for masked patch prediction: 80% MASK, 10% random, 10% original.
+        """
+        img_unnorm = orig_image * 0.5 + 0.5
+        _, _, ph, pw = self.patch_embed.proj.weight.shape
+        with torch.no_grad():
+            img_unnorm_patch = F.conv2d(
+                img_unnorm,
+                weight=torch.ones(3, 1, ph, pw).to(img_unnorm) / (ph * pw),
+                bias=None,
+                stride=(ph, pw),
+                padding=0,
+                groups=3,
+            )
+        labels = (
+            ((img_unnorm_patch * 255).long().flatten(start_dim=2, end_dim=3))
+            .permute(0, 2, 1)
+            .contiguous()
+        )
+
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape[:-1], 0.15)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = (
+            torch.bernoulli(torch.full(
+                labels.shape[:-1], 0.8)).bool() & masked_indices
+        )
+        feats[indices_replaced] = self.mask_token.to(feats)
+
+        return feats, labels
+
+    def visual_embed(self, _x, max_image_len=200, mask_it=False):
+        _, _, ph, pw = self.patch_embed.proj.weight.shape
+
+        # torch.Size([32, 3, 384, 384])->torch.Size([2, 768, 12, 12])
+        x = self.patch_embed(_x)
+        x_mask = (_x.sum(dim=1) != 0).float()[:, None, :, :]
+        x_mask = F.interpolate(x_mask, size=(x.shape[2], x.shape[3])).long()
+        x_h = x_mask[:, 0].sum(dim=1)[:, 0]
+        x_w = x_mask[:, 0].sum(dim=2)[:, 0]
+
+        B, C, H, W = x.shape
+        spatial_pos = (
+            self.pos_embed[:, 1:, :]
+            .transpose(1, 2)
+            .view(1, C, self.patch_dim, self.patch_dim)
+        )
+        pos_embed = torch.cat(
+            [
+                F.pad(
+                    F.interpolate(
+                        spatial_pos, size=(h, w), mode="bilinear", align_corners=True,
+                    ),
+                    (0, W - w, 0, H - h),
+                )
+                for h, w in zip(x_h, x_w)
+            ],
+            dim=0,
+        )  # torch.Size([2, 768, 12, 12])
+        """
+        /code:
+                pos_embed = torch.cat(
+            [
+                F.pad(
+                    F.interpolate(
+                        spatial_pos, size=(h, w), mode="bilinear", align_corners=True,
+                    ),
+                    (0, W - w, 0, H - h),
+                )
+                for h, w in zip(x_h, x_w)
+            ],
+            dim=0,
+        ) # torch.Size([2, 768, 12, 12])
+        这段代码的主要作用是生成位置编码。具体来说，pos_embed是一个四维的张量，其形状为[num_patches, embedding_dim, h, w]，其中num_patches表示图像中的patch数量，embedding_dim表示每个patch的embedding维度，h和w表示每个patch的高和宽。
+代码中的第一行将所有的spatial_pos（shape: [num_patches, 2]）通过插值方法等比例缩放到[h, w]的尺寸上，并使用F.pad函数在右边和下方填充0，使其形状为[1, 2, h, w]，即原位置信息被放置于一个高和宽都为h和w的小方块内。这样操作的目的是为了让每个小方块的位置信息区域对应每个patch的位置。
+接下来，for h, w in zip(x_h, x_w)将循环遍历每个patch的高和宽，并将上一步得到的位置信息小方块在适当位置上复制，填充整个位置编码张量，形状为[num_patches, embedding_dim, H, W]。
+最终得到的pos_embed的形状为[num_patches, embedding_dim, H, W]，其中num_patches等于batch_size * num_patches_per_img，即一个batch中所有图像的patch数量之和。
+        """
+        pos_embed = pos_embed.flatten(2).transpose(
+            1, 2)  # torch.Size([2, 144, 768])
+        # torch.Size([2, 768, 12, 12])->torch.Size([2, 144, 768])
+        x = x.flatten(2).transpose(1, 2)
         patch_index = (
             torch.stack(
                 torch.meshgrid(
